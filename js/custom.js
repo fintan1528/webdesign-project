e60aa79e6b658168586bf379a2334aa5dfc0936c
Fintan <fintanriordan1985@gmail.com>
2017-11-15 22:50:11 +0000

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -17,4 +17,4 @@ $('.panel-group').on('shown.bs.collapse', toggleIcon);
   js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.11';
   fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
-}
+
